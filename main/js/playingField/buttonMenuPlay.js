6225bc70a70da20faeeaab7a9cf6611014d1d232
Sergey Kiloshenko <sergeykiloshenko@gmail.com>
2021-03-28 19:59:42 +0300

    19 59 28.03.2021

--- main/js/playingField/buttonMenuPlay.js
+++ main/js/playingField/buttonMenuPlay.js
@@ -1,10 +1,90 @@
-let startSearchPlay = document.getElementById('searchPlay');
+let
+    startSearchPlay = document.getElementById('searchPlay'),
+    cancelSearchPlay = document.getElementById('unSearchPlay'),
+    preGameScreenNamePokeball = document.getElementById('playCntNamePB'),
+    playCntTimerWait = document.getElementById('playCntTimerWait'),
+    preGameScreenImgLayoutPokeball = document.querySelectorAll('#playCnt > div > div > img');
 
-PlayMenuBtn.addEventListener('click', function() {
+socket.on('youInGame', function (dataInGame) {
+    console.log(dataInGame);
+    ResetCountTimeWait();
+});
 
-})
+PlayMenuBtn.addEventListener('click', function () {
+
+    for (let i = 0; i < activePokeball.length; i++) {
+        const el = activePokeball[i];
+        if (el.style.border == '1px solid black') {
+            preGameScreenNamePokeball.innerHTML = el.namePB;
+            for (let j = 0; j < el.pokemons.name.length; j++) {
+                const elp = el.pokemons.name[j];
+                let imgPreGameScreen = Sandbox.find(item => item.name == elp);
+                preGameScreenImgLayoutPokeball[j].src = imgPreGameScreen.body;
+                preGameScreenImgLayoutPokeball[j].alt = imgPreGameScreen.name;
+
+            }
+            break;
+        }
+    }
+});
+
+let
+    secondsCTW = 0,
+    minuteCTW = 0,
+    countTimeWait = '00:00';
+
+function WorkCountTimeWait(block) {
+    let time = setInterval(() => {
+        ++secondsCTW;
+        if (secondsCTW >= 60) {
+            secondsCTW = 0;
+            ++minuteCTW;
+        }
+        if (minuteCTW < 10 && secondsCTW < 10 && secondsCTW < 60) {
+            countTimeWait = `0${minuteCTW}:0${secondsCTW}`;
+        }
+        if (minuteCTW >= 10 && secondsCTW < 10 && secondsCTW < 60) {
+            countTimeWait = `${minuteCTW}:0${secondsCTW}`;
+        }
+        if (minuteCTW < 10 && secondsCTW >= 10 && secondsCTW < 60) {
+            countTimeWait = `0${minuteCTW}:${secondsCTW}`;
+        }
+        if (minuteCTW == 10) {
+            clearInterval(time);
+            secondsCTW = 0;
+            minuteCTW = 0;
+            countTimeWait = '';
+            ResetCountTimeWait();
+        }
+        block.innerHTML = countTimeWait;
+    }, 1000);
+}
+
+function ResetCountTimeWait() {
+    cancelSearchPlay.style.display = 'none';
+    startSearchPlay.style.display = 'block';
+}
 
 startSearchPlay.addEventListener('click', () => {
 
-    socket.emit('ImReadyPlay', namePlayerHref);
+    let dataForDwnldGame = new Array();
+
+    for (let i = 0; i < preGameScreenImgLayoutPokeball.length; i++) {
+        dataForDwnldGame.push(preGameScreenImgLayoutPokeball[i].alt);
+    }
+
+    socket.emit('ImReadyPlay', [namePlayerHref, dataForDwnldGame]);
+
+    startSearchPlay.style.display = 'none';
+    cancelSearchPlay.style.display = 'block';
+    playCntTimerWait.innerHTML = '00:00';
+    WorkCountTimeWait(playCntTimerWait);
+});
+
+cancelSearchPlay.addEventListener('click', () => {
+    minuteCTW = 10;
+});
+
+socket.on('event', function (data) {
+    console.log(data);
 });
--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-const express = require('express'),
+const
+    express = require('express'),
     app = express(),
     httpsOptions = require('./keys/https'),
     https = require('https').Server(httpsOptions, app),
     io = require('socket.io')(https),
     dataChar = require('./Data/DataChar.json'),
     generator = require('generate-password'),
-    pgp = require("pg-promise")( /*options*/ );
+    pgp = require("pg-promise")( /*options*/);
 
 const cn = {
     host: 'localhost',
@@ -18,7 +19,9 @@ const db = pgp(cn);
 
 app.use("/", express.static(__dirname + "../../auth"));
 app.use("/Conflict", express.static(__dirname + "../../409"));
+
 let arrPokemonAccess = new Array(151);
+
 arrPokemonAccess = [
     '1', '0', '0', '1', '0', '0', '1', '0', '0', '1', '0', '0', '1', '0', '0', '1', '0', '0', '1', '0', '0', '0', '0', '0', '0',
     '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
@@ -29,7 +32,7 @@ arrPokemonAccess = [
 ];
 
 const urlPars = express.urlencoded({ extended: false });
-app.post("/reg", urlPars, function(req, res) {
+app.post("/reg", urlPars, function (req, res) {
     const token = generator.generate({
         length: 15,
     });
@@ -38,27 +41,27 @@ app.post("/reg", urlPars, function(req, res) {
         queryOn = 'INSERT INTO users(nickname, password, email, datereg, token, accesspokemon) VALUES($1, $2, $3, $4, $5, $6) RETURNING nickname, token;';
 
     db.one(query, [req.body.regNick])
-        .then(function(data) {
+        .then(function (data) {
             console.log(data, req.body.regNick);
             res.redirect(302, "/");
         })
-        .catch(function() {
+        .catch(function () {
             db.query(queryOn, [req.body.regNick, req.body.regPass, req.body.regEmail, new Date(), token, arrPokemonAccess])
-                .then(function(data) {
+                .then(function (data) {
                     console.log(data);
 
                     app.use(`/game/${token}/${req.body.regNick}`, express.static(__dirname + "../../main"));
                     res.redirect(301, `../game/${token}/${req.body.regNick}`);
                 })
-                .catch(function(error) {
+                .catch(function (error) {
                     console.log("ERROR:", error);
                 });
         });
 });
-app.post("/auth", urlPars, function(req, res) {
+app.post("/auth", urlPars, function (req, res) {
     const queryIn = 'SELECT * FROM users WHERE nickname = $1 AND password = $2 AND online = $3 LIMIT 1;';
     db.one(queryIn, [req.body.authNick, req.body.authPass, false])
-        .then(function(data) {
+        .then(function (data) {
             app.use(`/game/${data.token}/${data.nickname}`, express.static(__dirname + "../../main"));
             res.redirect(301, `../game/${data.token}/${req.body.authNick}`);
         })
@@ -68,7 +71,10 @@ app.post("/auth", urlPars, function(req, res) {
         })
 });
 
-let AllActiveUsers = new Array();
+let
+    AllActiveUsers = new Array(),
+    countForRooms = 0,
+    tempRoom;
 
 io.on('connection', socket => {
     console.log(`${socket.handshake.query.name} connected`);
@@ -76,13 +82,13 @@ io.on('connection', socket => {
     const
         changeOnline = 'UPDATE users SET online = $2 WHERE nickname = $1 RETURNING nickname, online, accesspokemon, datapokeball;';
     db.one(changeOnline, [OnUserTemp, true])
-        .then(function(data) {
+        .then(function (data) {
             socket.send(dataChar);
             socket.emit('accessPokemon', data.accesspokemon);
             socket.emit('downloadPokeball', data.datapokeball);
             console.log(data.nickname, data.online);
         })
-        .catch(function(error) {
+        .catch(function (error) {
             console.log("ERROR:", error);
         });
 
@@ -92,30 +98,65 @@ io.on('connection', socket => {
 
     // Рабочая область для передачи сообщений в процессе сессии приложения через {Socket.io}
 
-    socket.on('payPokemonForPlayer', function(PayPokForPlayer) {
+    socket.on('payPokemonForPlayer', function (PayPokForPlayer) {
         addPokAccesPokemon = `UPDATE users SET accesspokemon[${PayPokForPlayer.id}] = $2 WHERE nickname = $1 RETURNING nickname, accesspokemon;`;
         db.one(addPokAccesPokemon, [`${PayPokForPlayer.name}`, 1])
-            .then(function(data) {
+            .then(function (data) {
                 console.log('покупка:', data.nickname);
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 console.log("ERROR:", error);
             });
     });
 
-    socket.on('dataPkgPokeball', function(savePkgThePok) {
+    socket.on('dataPkgPokeball', function (savePkgThePok) {
         savePokForUser = 'UPDATE users SET datapokeball = $2 WHERE nickname = $1 RETURNING nickname, datapokeball;';
         db.one(savePokForUser, [`${savePkgThePok.name}`, savePkgThePok.data])
-            .then(function(data) {
+            .then(function (data) {
                 console.log('save:', data.nickname);
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 console.log("ERROR:", error);
             });
 
     });
 
     // Рабочая область для загрузки зала ожидания
+
+    socket.on('ImReadyPlay', function (dataFromPlayer) {
+        console.log(dataFromPlayer[0], dataFromPlayer[1]);
+
+        switch (countForRooms) {
+            case 0:
+                console.log(countForRooms);
+                tempRoom = `${dataFromPlayer[0]} Room${countForRooms}`;
+                socket.join(tempRoom);
+                countForRooms++;
+                console.log(tempRoom, socket.id, countForRooms);
+                break;
+            case 1:
+                socket.join(tempRoom);
+                console.log(tempRoom, socket.id, countForRooms);
+                io.to(tempRoom).emit('event', 'its a work');
+                countForRooms = 0;
+                break;
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -125,7 +166,7 @@ io.on('connection', socket => {
         let OffUserTemp = socket.handshake.query.name.trim();
         const changeOffline = 'UPDATE users SET online = $2 WHERE nickname = $1 RETURNING nickname, online;';
         db.one(changeOffline, [OffUserTemp, false])
-            .then(function(data) {
+            .then(function (data) {
                 console.log(data);
                 for (let i = 0; i < AllActiveUsers.length; i++) {
                     if (OffUserTemp == AllActiveUsers[i].handshake.query.name.trim()) {
@@ -134,7 +175,7 @@ io.on('connection', socket => {
                     }
                 }
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 console.log("ERROR:", error);
             });
     });
